3300859a4359f02e54493c49fc17091dcce07367
Ivriel <gunawanivriel@gmail.com>
2025-12-02 18:12:35 +0700

    cursor pagination

--- tests/Feature/QueryBuilderTest.php
+++ tests/Feature/QueryBuilderTest.php
@@ -428,6 +428,23 @@ class QueryBuilderTest extends TestCase
         $page++;
       
         }
-       
+    }
+
+    public function testCursorPagination()
+    {
+        $this->insertCategories();
+        $cursor = "id";
+        while(true){
+            $paginate = DB::table("categories")->orderBy("id")->cursorPaginate(perPage:2,cursor:$cursor);
+            foreach($paginate->items() as $item) {
+                self::assertNotNull($item);
+                Log::info(json_encode($item));
+            }
+
+            $cursor = $paginate->nextCursor();
+            if($cursor == null) {
+                break;
+            }
+        }
     }
 }
